4a37cb3fea0d882f9cbc59db7ecf3f2cee5c6b00
Holger <hdecarne@gmail.com>
2024-01-13 03:51:37 +0100

    Add testing code

--- certs/acme/config_test.go
+++ certs/acme/config_test.go
@@ -16,4 +16,13 @@ func TestLoadConfig(t *testing.T) {
 	config, err := acme.LoadConfig("./testdata/acme-test.yaml")
 	require.NoError(t, err)
 	require.NotNil(t, config)
+	require.Equal(t, 2, len(config.Providers))
+	provider1 := config.Providers["Test1"]
+	require.NotNil(t, provider1)
+	require.Equal(t, "Test1", provider1.Name)
+	require.True(t, provider1.Enabled)
+	provider2 := config.Providers["Test2"]
+	require.NotNil(t, provider2)
+	require.Equal(t, "Test2", provider2.Name)
+	require.False(t, provider2.Enabled)
 }
